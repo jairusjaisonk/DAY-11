4685b198fabffcb39a39e1cacaad48d4faea7c1d
jairusjaisonk <jairusjaison.k@gmail.com>
2026-07-16 06:56:56 +0530

    Add files via upload

---
+++ day 11.py
@@ -0,0 +1,29 @@
+# RECURSIVE FUNCTION
+# def counttozero(n):
+#     print(n)
+#     if n == 0 :
+#         return
+    
+#     return counttozero(n-1)
+
+# counttozero(100)
+# RECURSION SUM OF 10 NUM 
+# def sum(n):
+#     if n==0 :
+#         return 0
+    
+#     return n + sum(n-1)
+
+# print (sum(10))
+
+
+# scope of a variable 
+# area in which it is recognised 
+
+n=("giberish")
+def myname():
+    name = "jaison"
+    return name 
+print(myname())
+
+# MODULES  // MODULAR PROGRAMINNG
